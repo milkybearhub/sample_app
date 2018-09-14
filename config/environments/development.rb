a619ce21824b1567e89ab9a7430c062ff8908ffe
Masatoshi <matcha_ordermasa@yahoo.co.jp>
2018-09-14 01:54:41 +0000

    Add account activation

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,7 +27,10 @@ Rails.application.configure do
   end
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :test
+  host = 'cd788e2a4483452690183e839af10d30.vfs.cloud9.ap-southeast-1.amazonaws.com.c9users.io'
+  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
 
   config.action_mailer.perform_caching = false
 
