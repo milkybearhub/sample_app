80fbc45f307b1867df892d1e8bfcfab9952e186d
Masatoshi <matcha_ordermasa@yahoo.co.jp>
2018-10-13 06:06:44 +0000

    Add inquiry pages and a background image

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 これは、次の教材で作られたサンプルアプリケーションです。   
 [*Ruby on Rails チュートリアル*](https://railstutorial.jp/)
 [Michael Hartl](http://www.michaelhartl.com/) 著
+一部の機能ををオリジナルで追加しています。
 
 ## ライセンス
 
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -24,9 +24,4 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_select "title", "About | #{@base_title}"
   end
   
-  test "should get contact" do
-    get contact_path
-    assert_response :success
-    assert_select "title", "Contact | #{@base_title}"
-  end
 end
